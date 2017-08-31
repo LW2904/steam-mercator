80158782a1eb6c83035abf8a0ad8708d8746c82a
LW2904 <8392143a@gmail.com>
2017-08-31 18:16:54 +0200

    Update chatMessage.

--- components/commands.js
+++ components/commands.js
@@ -13,12 +13,12 @@ Bot.prototype._cmdCheck = function (id, args) {
 
   if (args[1]) {
     let data = this.LevelParser.Calculate(parseInt(args[0]), parseInt(args[1]))
-    this.client.chatMessage(id, `Level ${args[0]} -> ${args[1]} would require ${data.Intermediate.Exp} EXP (${data.Intermediate.Sets} Sets) and therefore ${data.Intermediate.Keys} Key${data.Intermediate.Keys > 1 ? 's' : ''}.`)
+    this.client.chatMessage(id, `Level ${args[0]} -> ${args[1]} would require ${data.Intermediate.Exp} EXP (${data.Intermediate.Sets} Sets) and therefore about ${data.Intermediate.Keys} Key${data.Intermediate.Keys > 1 ? 's' : ''}.`)
   } else {
     level.get(id)
     .then(level => {
       let data = this.LevelParser.Calculate(level, parseInt(args[0]))
-      this.client.chatMessage(id, `Level ${level} -> ${args[0]} would require ${data.Intermediate.Exp} EXP (${data.Intermediate.Sets} Sets) and therefore ${data.Intermediate.Keys} Key${data.Intermediate.Keys > 1 ? 's' : ''}.`)
+      this.client.chatMessage(id, `Level ${level} -> ${args[0]} would require ${data.Intermediate.Exp} EXP (${data.Intermediate.Sets} Sets) and therefore about ${data.Intermediate.Keys} Key${data.Intermediate.Keys > 1 ? 's' : ''}.`)
     }).catch(err => {
       this.client.chatMessage(id, '(ノಠ益ಠ)ノ彡┻━┻ couldn\'t get steam level.')
       this.emit(`levelError`, err)
